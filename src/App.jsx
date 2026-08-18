83de6387c8533a55f3b7814b5a3bfe659ab395f9
Juls <orbistajuliana26@gmail.com>
2026-08-18 10:52:55 +0800

    draft & navbar

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
 
       <nav className="navbar">
         <h2>React Activity Portal</h2>
+        <h2>palitan na lang yung UI draft lang ito </h2>
 
         <div className="nav-links">
           <Link to="/">Home</Link>
